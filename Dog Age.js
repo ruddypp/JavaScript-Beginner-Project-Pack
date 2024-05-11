091e659a500401c718b2db07ba9284d372f9d135
Rudy Paningal <149013690+ruddypp@users.noreply.github.com>
2024-05-11 18:58:21 +0700

    Add files via upload

---
+++ Dog Age.js
@@ -0,0 +1,12 @@
+ // membuat variable umur
+ const myAge = 21;
+ // variable tahun
+ let earlyYears = 2;
+ // menghitung tahun
+ earlyYears *= 10.5;
+ let laterYears = myAge - 2;
+ laterYears *= 4;
+ console.log(earlyYears);
+myAgeInDogYears = earlyYears + laterYears;
+let myName = 'RUDY PANINGAL'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is, ${myAgeInDogYears} years old in dog years`);
